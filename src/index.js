d93fb03405ce9c148ebb27d124ee56e811fea200
AnnabelNKir <nkiroteannabel@gmail.com>
2022-08-24 17:49:58 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,77 +1,155 @@
 // Code here
 // DOMContentLoaded event
-document.addEventListener("DOMContentLoaded",() => {
+document.addEventListener("DOMContentLoaded", () => {
+  const mainBeerContainer = document.querySelector("main")
+  const beerList = document.getElementById("beer-list");
 
-    const beerTitle = document.getElementById("beer-name")
-    const beerImage = document.getElementById("beer-image")
-    const beerDesc = document.getElementById("beer-description")
-    const reviewList = document.getElementById("review-list")
-
-      // View the first beer's details, including its name, image, description, and reviews, when the page loads.
-      // fetch beer data from the server
-
-      fetch("http://localhost:3000/beers")
-      .then((res)=>res.json())
-      .then((beers)=>{
-
-        // display beer one
-        beerTitle.textContent = beers[0].name
-        beerImage.src = beers[0].image_url
-        beerDesc.textContent = beers[0].description
-
-        //reviewList.textContent = beers[0].reviews
-        beers[0].reviews.forEach(review => {
-            let reviews = document.createElement('li')
-            reviews.style.cursor ='pointer'
-            reviews.innerText = review
-            reviewList.appendChild(reviews)
-            reviews.addEventListener("click", (e) => {
-              e.preventDefault()
-              review.remove()
-            })
-          })
-
-          // call for loop to iterate over the array for each element in the array.
-        beers.forEach(beer =>{
-            const beerList = document.getElementById("beer-list")
-            const list = document.createElement("li")
-
-            // append beer list to the 'nav'
-
-          list.innerText = beer.name
-          beerList.appendChild(list)  
-
-          // add event listener to activate beer  name, image, description and reviews.
-
-          list.addEventListener('click', () => {
-            const beerName = document.getElementById("beer-name")
-            beerName.innerText = beer.name
-            beerImage.src = beer.image_url
-            const beerDescription = document.getElementById("beer-description")
-            beerDescription.innerText = beer.description
-            reviewList.textContent = beer.reviews
-            reviewList.textContent = beer[0].reviews
-            const reviewForm = document.querySelector("#review-form")
-            reviewForm.addEventListener('submit', (e) => {
-                e.preventDefault()
-            const marketReview = document.getElementById("review").value
-            const newReview = document.createElement("li")
-            newReview.style.cursor = "pointer"
-            newReview.innerText = marketReview
-
-            // When the description form is submitted the beer's description should be updated on the page.
-            
-            reviewList.appendChild(newReview)
-            reviewForm.reset()
-            newReview.addEventListener('click', (e) =>{
-                e.preventDefault()
-                
-                // Remove a review from the page when it is clicked.       
-                newReview.remove()
-            })
-            })
-          })
-        })
+  fetch("http://localhost:3000/beers")
+  .then(response => response.json())
+  .then(beerData => {
+      beerList.innerHTML = ''
+      beerData.forEach(item => {
+          createDOM(item)
       })
   })
+
+  function createDOM(beerDataItem) {
+      let beer = document.createElement("li")
+      beer.addEventListener("click", function() {
+          displayBeerInfo(beerDataItem)
+      })
+      beer.innerText = beerDataItem.name
+      beerList.appendChild(beer)
+
+  }
+
+  function fetchBeer(id) {
+      if(typeof(id) !== 'number') id = parseInt(id)
+      fetch(`http://localhost:3000/beers/${id}`)
+      .then(response => response.json())
+      .then(beer => {
+          displayBeerInfo(beer)
+      })
+  }
+
+  fetchBeer(1)
+
+  function displayReviews(beer, reviewContainer) {
+      beer.reviews.forEach(item => {
+          let review = document.createElement("li")
+          review.innerText = item
+          reviewContainer.append(review)
+          review.addEventListener("click", function(){
+              review.remove()
+          })
+      })
+  }
+
+
+  function displayBeerInfo(beer) {
+      mainBeerContainer.innerHTML = '';
+
+      const beerContainer = document.createElement("div")
+      beerContainer.setAttribute("class", "beer-details")
+
+      const detailHtml = `
+          <h2 id="beer-name">${beer.name}</h2>
+          <img
+          id="beer-image"
+          alt="Beer Name Goes Here"
+          src="${beer.image_url}"
+          />
+          <p>
+          <em id="beer-description">${beer.description}</em>
+          </p>
+          <form id="description-form">
+          <label for="description">Edited Description:</label>
+          <textarea id="description"></textarea>
+          <button type="submit">Update Beer</button>
+          </form>
+          <h3>Customer Reviews</h3>
+          <ul id="review-list">
           
+          </ul>
+          <form id="review-form">
+          <label for="review">Your Review:</label>
+          <textarea id="review"></textarea>
+          <button type="submit">Add review</button>
+          </form>
+      `
+
+      beerContainer.innerHTML = detailHtml
+
+      const decriptionContainer = beerContainer.querySelector("#beer-description")
+
+      let descriptionTextArea = beerContainer.querySelector("#description")
+      descriptionTextArea.value = beer.description
+
+      let reviewsContainer = beerContainer.querySelector("#review-list")
+      displayReviews(beer, reviewsContainer)
+
+      mainBeerContainer.appendChild(beerContainer)
+      const id = beer.id
+
+      const reviewForm = beerContainer.querySelector("#review-form")
+      reviewForm.addEventListener("submit", function(e){
+          e.preventDefault()
+          const entry = reviewForm["review"].value
+          submitReview(beer, id, entry)
+          reviewsContainer.innerHTML = ''
+          displayReviews(beer, reviewsContainer)
+          reviewForm.reset()
+      })
+
+      const editDescriptionForm = beerContainer.querySelector("#description-form")
+      editDescriptionForm.addEventListener("submit", function(e) {
+          e.preventDefault()
+          const editedDesc = descriptionTextArea.value
+          submitEditedBeerDescription(beer, id, editedDesc, decriptionContainer)
+      })
+  }
+
+  function submitReview(beer, id, entry) {
+      beer.reviews.push(entry)
+      const reviews = beer.reviews
+      const data = {
+          id: id,
+          name: beer.name,
+          description: beer.description,
+          "image_url": beer.image_url,
+          reviews: reviews
+      }
+      fetch(`http://localhost:3000/beers/${id}`, {
+          method: 'PATCH',
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+      .then(result => result)
+      
+  }
+
+  function submitEditedBeerDescription(beer, id, editedDescription, descContainer) {
+      const data = {
+          id: id,
+          name: beer.name,
+          description: editedDescription,
+          "image_url": beer.image_url,
+          reviews: beer.reviews
+      }
+      fetch(`http://localhost:3000/beers/${id}`, {
+          method: 'PATCH',
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+      .then(result => {
+          descContainer.innerText = ''
+          descContainer.innerText = result.description
+      })
+  }
+})
